da859cbf0b7ebc31bbfe0839e66d9a60f537a507
KENDRE JANHAVI BHAGWAT <141988507+janhavikendre@users.noreply.github.com>
2025-01-14 23:53:56 +0530

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -2,15 +2,16 @@
 import React from "react";
 import { StickyScroll } from "./ui/StickyScroll";
 import Image from "next/image";
+import { Github ,Webhook,BookOpen} from 'lucide-react';
 
 const content = [
   {
-    title: "Real-time Updates",
+    title: "Webhook Integration",
     description:
-      "I implement real-time changes in most of my projects, ensuring that all updates are reflected instantly across all connected clients. By leveraging tools like WebSockets and Pusher, I ensure efficient and transparent workflows, whether it’s in collaborative tools or group chat applications.",
+      "I specialize in implementing webhook systems that enable automated interactions between different services and applications. By setting up reliable webhook endpoints, I ensure seamless data flow and event-driven communication between various platforms, making integration processes efficient and automated.",
     content: (
       <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-        Collaborative Editing
+        <Webhook size={80} />
       </div>
     ),
   },
@@ -20,13 +21,7 @@ const content = [
       "In my projects, I integrate version control systems like GitHub to ensure smooth collaboration with other developers and prevent the chaos of manual updates. From handling large codebases to maintaining clean version histories, my approach to version control is seamless and efficient",
     content: (
       <div className="h-full w-full  flex items-center justify-center text-white">
-        <Image
-          src="https://raw.githubusercontent.com/adrianhajdin/portfolio/aa3fc6de5066925313070cbb5bdce9d85cc89241/public/b1.svg"
-          width={300}
-          height={300}
-          className="h-full w-full object-cover"
-          alt="linear board demo"
-        />
+        <Github size={80} />
       </div>
     ),
   },
@@ -35,8 +30,8 @@ const content = [
     description:
       "I am not only focused on building scalable applications but also on making them user-friendly. Whether you're a team looking to build collaborative platforms or an individual needing to streamline your workflows, I can help create tailored solutions that meet your specific needs.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
-        Version control
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <BookOpen   size={80} />
       </div>
     ),
   },
