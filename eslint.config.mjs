74c1bc295b105518c5c9c728bb4c1b18ff6cb061
janhavikendre <janhavikendre717@gmail.com>
2025-01-14 19:37:48 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const _dirname = dirname(_filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -11,6 +11,25 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "@next/next/no-img-element": "off",
+      "@next/next/no-head-element": "off",
+      "@next/next/google-font-display": "off",
+      "@next/next/google-font-preconnect": "off",
+      "@next/next/next-script-for-ga": "off",
+      "@next/next/no-sync-scripts": "off",
+      "@next/next/no-css-tags": "off",
+      "@next/next/no-page-custom-font": "off",
+      "@next/next/no-title-in-document-head": "off",
+      "@next/next/no-typos": "off",
+      "@next/next/no-unwanted-polyfillio": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars":"off",
+      "@typescript-eslint/no-unused-expressions":"off"
+    }
+  }
 ];
 
 export default eslintConfig;
